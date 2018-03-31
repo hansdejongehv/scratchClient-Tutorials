eabae87ca48e42832f8431835f59563eb50fc3c3
Hans de Jong <hans.github@hanselma.nl>
2018-03-31 19:24:51 +0200

    with fixed version for tornado

--- Install/InstallScratchClient.sh
+++ Install/InstallScratchClient.sh
@@ -72,8 +72,8 @@ case "$InstallScratchClient" in
 				# get install packages that scratchClient needs ...
 		sudo apt-get install python-pip python-dev python-smbus 
 		sudo apt-get install python3-pip python3-dev python3-smbus 
-		sudo pip install tornado mako==1.0.6 spidev pyserial intelhex
-		sudo pip3 install tornado mako==1.0.6 spidev pyserial intelhex
+		sudo pip install tornado==4.5.2 mako==1.0.6 spidev pyserial intelhex
+		sudo pip3 install tornado==4.5.2 mako==1.0.6 spidev pyserial intelhex
 				# ... and the same for the rest of the needed packages.
 
 		# install the extension for Scratch2
